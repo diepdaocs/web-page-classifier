57a52ec33e97c55c16f516eacc78acad979aee8d
Diep Dao <diepdao12892@gmail.com>
2016-04-15 20:15:24 +0700

    model evaluation

--- nlp/evaluation_model.py
+++ nlp/evaluation_model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import classification_report
+from nlp.predict_data import PredictWebPageType
 from util.utils import get_logger
 import pandas as pd
 
@@ -6,10 +7,11 @@ __author__ = 'diepdt'
 
 
 class WebPageTypeModelEvaluation(object):
-    def __init__(self, urls, storage):
+    def __init__(self, urls, storage, model_name):
         self.logger = get_logger(self.__class__.__name__)
         self.urls = urls
         self.storage = storage
+        self.classifier = PredictWebPageType('../model/' + model_name)
 
     def load_test_data(self):
         result = []
@@ -21,5 +23,12 @@ class WebPageTypeModelEvaluation(object):
         return pd.DataFrame(result, index=False, columns=['content', 'type'])
 
     def evaluate(self):
+        predicts = {p['url']: p['type'] for p in self.classifier.predict(self.urls)}
         data = self.load_test_data()
-        return classification_report(data['content'].values, data['type'].values)
+        data['predict'] = ''
+        for row in data.iterrows():
+            row['predict'] = predicts['']
+
+        x = data['type'].values
+        y_true = data['content'].values
+        return classification_report(, )
