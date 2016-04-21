42f5921ab86a4ff4b5c0e619167f1de4a75ca7e6
Diep Dao <diepdao12892@gmail.com>
2016-04-21 08:11:06 +0700

    check empty page

--- parser/content_getter.py
+++ parser/content_getter.py
@@ -14,6 +14,10 @@ class ContentGetter(object):
         # extract content from pages
         self.logger.debug('Start extract pages: %s' % urls)
         for url, page in result.items():
+            if not page['content']:
+                page['content'] = url
+                continue
             page['content'] = ', '.join(c for c in [url, self.extractor.process(page['content'])] if c)
+
         self.logger.debug('End extract pages: %s' % urls)
         return result
